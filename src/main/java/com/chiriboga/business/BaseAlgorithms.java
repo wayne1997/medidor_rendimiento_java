b301437e8874e2068b85021cf5da9853be539f0e
alex <jchiriboga15@gmail.com>
2025-07-13 21:40:37 +0200

    Project tracker added some basic algorithms

---
+++ src/main/java/com/chiriboga/business/BaseAlgorithms.java
@@ -0,0 +1,82 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.chiriboga.business;
+
+/**
+ *
+ * @author Alexander Chiriboga
+ */
+public class BaseAlgorithms {
+
+    public long totalSumLoopAlg(long size) {
+        System.out.println("Size: " + size);
+        long sum = 0;
+        for (long i = 1; i <= size; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+   
+    public long algebraicSumAlg(long num){
+        long total = 0;
+        total = (num * (num + 1)) / 2;
+        return total;
+    }
+    
+    public String CountBottomToTop(long point){
+        for (long i = 0; i < point; i++) {
+            System.out.println("Valor: " + i);
+        }
+        for (long j = point; j >= 0; j--) {
+            System.out.println("Valor: " + j);
+        }
+        return "¡Algoritmo terminado! 🔥 \n He contado desde el incio hasta el final y viceversa.";
+    }
+    
+    
+    public void showMatrixIndices(int index) {
+        for(int i = 0; i < index; i++){
+            for(int j = 0; j < index; j++){
+                System.out.println("Posiciones de la matriz:\t" + i + " " + j);
+            }
+        }
+    }
+    
+    public String getTotalSumLoopAlg(){
+        return """
+               public int SumaConBucle(int size) {
+                    int sum = 0;
+                    for (int i = 1; i < size; i++) {
+                        sum += i;
+                    }
+                    return sum;
+                }
+               """;
+    }
+    
+    public String getTotalSumAlgebraicAlg(){
+        return """
+               public int SumaAlgebraica(int num){
+                    int total = 0;
+                    total = (num * (num + 1)) / 2;
+                    return total;
+                }
+               """;
+    }
+    
+    public String getDoubleLoopAlg(){
+        return """
+               public String CountBottomToTop(long point){
+                       for (long i = 0; i < point; i++) {
+                           System.out.println("Valor: " + i);
+                       }
+                       for (long j = point; j >= 0; j--) {
+                           System.out.println("Valor: " + j);
+                       }
+                       return "¡Algoritmo terminado! 🔥 He contado desde el incio hasta el final y viceversa ";
+                }
+               """;
+    }
+}
